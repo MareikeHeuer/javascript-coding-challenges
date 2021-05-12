6043c1bad06433f19fac4decabc58216f71d5ba6
Mareike Heuer <mareike.heuer@hotmail.com>
2021-05-12 11:05:11 +0200

    Developer Skills & Editor Setup - Coding Challenge 1 complete

--- scripts.js
+++ scripts.js
@@ -310,6 +310,8 @@ length of the array (because that's the number of elements)
 4.3. Call the function with the 'totals' array
 */
 
+/*
+
 const calcTip = (bill) =>
   bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 
@@ -336,3 +338,41 @@ const calcAverage = function (arr) {
 };
 
 console.log(calcAverage(totals));
+*/
+
+/////////////////////////////////////
+// Developer Skills & Editor Setup //
+
+/* Coding Challenge #1
+Given an array of forecasted maximum temperatures, the thermometer displays a
+string with the given temperatures. Example: [17, 21, 23] will print "... 17ºC in 1
+days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+Your tasks:
+1. Create a function 'printForecast' which takes in an array 'arr' and logs a
+string like the above to the console. Try it with both test datasets.
+2. Use the problem-solving framework: Understand the problem and break it up
+into sub-problems!
+Test data:
+§ Data 1: [17, 21, 23]
+§ Data 2: [12, 5, -5, 0, 4] */
+
+/* Pseudocode
+1. store data in variables
+2. write function that takes arr as parameter
+3. define empty string
+4. iterate over array with loop, then add information into string
+5. console.log the result
+*/
+
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+
+const printForecast = function (arr) {
+  let str = "";
+  for (let i = 0; i < arr.length; i++) {
+    str += `${arr[i]}ºC in ${i + 1} days... `;
+  }
+  console.log("... " + str);
+};
+
+printForecast(data1);
